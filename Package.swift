6771855ecd8c3a41fc8746d1d8381f7083d54812
Tarlan Ismayilsoy <terlan_ismayilsoy@hotmail.com>
2023-12-03 21:53:24 +0400

    Initial commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CircularReveal",
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -16,8 +17,5 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "CircularReveal"),
-        .testTarget(
-            name: "CircularRevealTests",
-            dependencies: ["CircularReveal"]),
     ]
 )
--- Sources/CircularReveal/CircularReveal.swift
+++ Sources/CircularReveal/CircularReveal.swift
@@ -1,2 +1,58 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  CircularReveal.swift
+//
+//  Created by Tarlan Ismayilsoy on 03.12.23.
+//
+
+import SwiftUI
+
+// TODO: make it work for a bool
+// TODO: make it work for a binding item
+// TODO: also animate dismiss action
+struct CircularReveal<V>: ViewModifier where V: View {
+    @Binding var isPresented: Bool
+    var viewToReveal: () -> V
+    
+    func body(content: Content) -> some View {
+        content
+            .onChange(of: isPresented) { _ in
+                UIView.setAnimationsEnabled(!isPresented)
+            }
+            .fullScreenCover(isPresented: .constant(true), content: { // TODO: remove constant
+                viewToReveal()
+                    .onAppear {
+                        UIView.setAnimationsEnabled(true)
+                    }
+                    .mask (
+                        Circle()
+                            .frame(width: 100)
+                    )
+            })
+    }
+}
+
+public extension View {
+    func circularReveal<Content>(isPresented: Binding<Bool>,
+                        @ViewBuilder content: @escaping () -> Content) -> some View where Content : View {
+        modifier(CircularReveal(isPresented: isPresented, viewToReveal: content))
+    }
+}
+
+//extension View {
+//    /// Works like mask, but in reverse
+//    @inlinable func reverseMask<Mask: View>(
+//        alignment: Alignment = .center,
+//        @ViewBuilder _ mask: () -> Mask
+//    ) -> some View {
+//        self.mask(
+//            ZStack {
+//                Rectangle()
+//                    .ignoresSafeArea()
+//                
+//                mask()
+//                    .blendMode(.destinationOut)
+//            }
+//        )
+//    }
+//}
+
